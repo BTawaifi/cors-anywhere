ee0935f5162eac5ed97ade6fd328551f2f48cc83
Boutros Tawaifi <52285931+BTawaifi@users.noreply.github.com>
2021-04-19 20:41:32 +0300

    Added Website To Whitelist

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+//var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = ['https://btawaifi.github.io/#/btanime','https://btawaifi.github.io','https://btawaifi.github.io/#/'];
 function parseEnvList(env) {
   if (!env) {
     return [];
